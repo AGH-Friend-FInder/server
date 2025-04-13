efb0c0f1cded74410360e512a852760ee5c58320
Mikosztyla <mikosztyla@gmail.com>
2025-04-13 14:06:41 +0200

    Logging

--- src/main/java/agh/pin/pals/server/controllers/CurrentPinsController.java
+++ src/main/java/agh/pin/pals/server/controllers/CurrentPinsController.java
@@ -5,6 +5,8 @@ import agh.pin.pals.server.models.CurrentPins;
 import agh.pin.pals.server.models.Group;
 import agh.pin.pals.server.services.CurrentPinsService;
 import agh.pin.pals.server.services.GroupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/pin")
 public class CurrentPinsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CurrentPinsController.class);
     private final CurrentPinsService currentPinsService;
 
     @Autowired
@@ -27,21 +30,25 @@ public class CurrentPinsController {
 
     @PostMapping()
     public CurrentPins createCurrentPin(@RequestBody final CurrentPinsDTO currentPinsDTO) {
+        logger.info("Creatign pin: " + currentPinsDTO.getPin());
         return currentPinsService.createCurrentPin(currentPinsDTO);
     }
 
     @GetMapping("/{id}")
     public CurrentPins getCurrentPinById(@PathVariable Long id) {
+        logger.info("Getting pin: " + id);
         return currentPinsService.getCurrentPinById(id);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCurrentPinById(@PathVariable Long id) {
+        logger.info("Deleting pin: " + id);
         currentPinsService.deleteCurrentPinById(id);
     }
 
     @GetMapping("/visible/{id}")
     public List<CurrentPins> getVisibleCurrentPins(@PathVariable Long id) {
+        logger.info("Getting visible pins: " + id);
         return currentPinsService.getVisibleCurrentPins(id);
     }
 
--- src/main/java/agh/pin/pals/server/controllers/UserController.java
+++ src/main/java/agh/pin/pals/server/controllers/UserController.java
@@ -8,6 +8,8 @@ import agh.pin.pals.server.repositories.UserRepository;
 import agh.pin.pals.server.services.GroupService;
 import agh.pin.pals.server.services.GroupService;
 import agh.pin.pals.server.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
@@ -32,16 +35,19 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        logger.info("Creating user: " + user.getId());
         return userService.createUser(user);
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
+        logger.info("Getting user: " + id);
         return userService.getUserById(id);
     }
 
     @PostMapping("/register")
     public ResponseEntity<Object> createUser(@Valid @RequestBody UserDTO userDTO) {
+        logger.info("Registering user: " + userDTO.getUsername());
         try {
             User createdUser = userService.createUser(userDTO);
             return ResponseEntity.ok(createdUser);
@@ -52,6 +58,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<Object> loginUser(@Valid @RequestBody UserDTO userDTO) {
+        logger.info("Logging user: " + userDTO.getUsername());
         try {
             User user = userService.loginUser(userDTO);
             return ResponseEntity.ok(user);
@@ -62,6 +69,7 @@ public class UserController {
 
     @PostMapping("/{user_id}/{group_id}")
     public ResponseEntity<Object> addUserToGroup(@PathVariable Long user_id, @PathVariable Long group_id) {
+        logger.info("Adding user: " + user_id + " to group: " + group_id);
         try {
             User user = userService.addUserToGroup(user_id,group_id);
             return ResponseEntity.ok(user);
@@ -72,11 +80,13 @@ public class UserController {
 
     @DeleteMapping("/{user_id}/{group_id}")
     public void deleteUserFromGroup(@PathVariable Long user_id, @PathVariable Long group_id) {
+        logger.info("Deleting user: " + user_id + " from group: " + group_id);
         userService.removeUserFromGroup(user_id,group_id);
     }
 
     @GetMapping("/group/{id}")
     public List<Group> getUserGroups(@PathVariable Long id) {
+        logger.info("Getting groups for user: " + id);
         return userService.getUserGroups(id);
     }
 
